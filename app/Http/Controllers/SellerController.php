0320823d35fae6a49f45064a711d62b80bf51966
Calvin Kurniawan <calvineuy@gmail.com>
2025-10-21 19:08:33 +0700

    asd

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -6,7 +6,6 @@ use App\Models\Store;
 use App\Models\Product;
 use Illuminate\Support\Carbon;
 use App\Models\PreOrderCampaign;
-use Illuminate\Http\Client\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,7 +13,7 @@ class SellerController extends Controller
 {
     public function view_HomeSeller($account_code)
     {
-        $user   = Auth::user();
+        $user = Auth::user();
         $store = Store::where('account_code', $account_code)->first();
 
         if (!$store || $store->id !== $user->store_id) {
@@ -29,33 +28,28 @@ class SellerController extends Controller
     public function view_Product($account_code)
     {
         $store = Store::where('account_code', $account_code)->firstOrFail();
-
         $products = Product::where('store_id', $store->id)->get();
 
         return view('seller.product', compact('products', 'store'));
     }
 
-
     public function view_PreOrderList($account_code)
     {
-        $store = Store::where('account_code', $account_code)->first();
+        $store = Store::where('account_code', $account_code)->firstOrFail();
 
-        $poform = PreOrderCampaign::all()->map(function ($item) {
+        $poform = PreOrderCampaign::where('account_code', $account_code)->get()->map(function ($item) {
             $item->start_date = Carbon::parse($item->start_date)->format('j-M-Y');
-            $item->end_date   = Carbon::parse($item->end_date)->format('j-M-Y');
-
-            // tambahkan properti baru buat status
+            $item->end_date = Carbon::parse($item->end_date)->format('j-M-Y');
             $item->status_label = ($item->status == 1) ? 'Active' : 'Non Active';
-
             return $item;
+        });
 
-                return view('seller.preorder.preorderlist', compact('store','poform'));
+        return view('seller.preorder.preorderlist', compact('store', 'poform'));
     }
-
 
     public function view_PreOrderCreate($account_code)
     {
-        $store = Store::where('account_code', $account_code)->first();
+        $store = Store::where('account_code', $account_code)->firstOrFail();
         return view('seller.preorder.createpreorder', compact('store'));
     }
 }
